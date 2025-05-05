1ee91c47704b03dbdb3501296b77a5754452059b
penneytech <penneyp@hdsb.ca>
2025-05-05 09:40:07 -0400

    first commit.

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Instructions:
+
+Write a program that achieves the following: 
+
+1) Create an array with 5 elements in it, with values of your choice.
+2) Log all five elements to the console using console.log() methods.
+3) Choose one of the elements and change it's contents using an equals statement.
+4) Log the changed array to the console with a forEach() loop.
+
+Put your code under the comments below:
+*/
+
+
+// 1) Create an array with 5 elements in it, with values of your choice.
+
+
+// 2) Log all five elements to the console using console.log() methods.
+
+
+// 3) Choose one of the elements and change it's contents using an equals statement.
+
+
+// 4) Log all 5 elements of the changed array to the console with a forEach() loop.
+
+
